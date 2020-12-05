c26c23ea7a845a67463fdd6a4d6fb27e51d77034
lihx <2679656468@qq.com>
2020-12-05 19:33:59 +0800

    12.05

--- src/components/menu/MenuBar.vue
+++ src/components/menu/MenuBar.vue
@@ -171,7 +171,7 @@
                                 : AssessControlSrc.assess.deactive
                             "
                             class="control_items"
-                            @click.stop="algorithm_Task(itemRoute, indexRoute)"
+                            @click.stop="algorithm(itemRoute, indexRoute)"
                           />
 
                         <!-- 航线编辑按钮 -->
@@ -666,13 +666,13 @@ export default {
           this.loadRouteList(item, index);
         });
     },
-    algorithm(item, index) {
-      this.setAlgorithm([1, item]);
-    },
-    algorithm_Task(itemRoute, indexRoute){
+    // 新建评估
+    algorithm(itemRoute, indexRoute) {
       itemRoute.showAlorithm = !itemRoute.showAlorithm
+      console.log(itemRoute.showAlorithm,`当前是否打开`)
       this.setAlgorithm([1, itemRoute]);
     },
+    // 新增航线
     addTaskItem(item, index) {
       console.log(item, index, `item`);
       this.setRouteDialogOptions([1, item, index, false]);
@@ -750,7 +750,13 @@ export default {
     },
     // 查看评估配置参数
     AssessInfo(itemAssess, itemRoute, indexRoute, item, index) {
+      console.log(itemAssess,`itemAssess`)
       itemAssess.alorithm = !itemAssess.alorithm
+      if(itemAssess.alorithm) {
+        this.setAlgorithm([2, itemAssess]);
+      } else {
+        this.setAlgorithm([0, {}]);
+      }
     },
     // 请求任务列表
     loadTaskList() {
