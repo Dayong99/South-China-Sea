c246c5106f276341aa4fe8066f346b7ae43c7e28
huangh <2568900371@qq.com>
2021-01-05 12:41:38 +0800

    105h

--- public/static/config.js
+++ public/static/config.js
@@ -1,6 +1,6 @@
 window.globalConfig = {
-    // baseURL: 'http://192.168.2.112:8088/warship'
-    baseURL: 'http://192.168.1.142:8088/warship'
+    baseURL: 'http://192.168.2.112:8088/warship'
+    // baseURL: 'http://192.168.1.142:8088/warship'
 
     // baseURL:'http://192.168.1.122:8301/warship'
     // baseURL: 'http://192.168.1.122:8088/warship'
